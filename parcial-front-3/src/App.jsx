126e4a7c63456e9b8a64092ed917fc462d722002
emi-dev28 <emilianocellilli00@gmail.com>
2023-08-14 22:44:52 -0300

    Pre-parcial

---
+++ parcial-front-3/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Form from './Components/Form'
+
+function App() {
+
+
+  return (
+    <>
+      <Card/>
+      <Form/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ parcial-front-3/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <h3>Soy una card </h3>
+    </div>
+  )
+}
+
+export default Card
